05ab6d68ed3d3ef04c8808cc06f496496c6ac58d
Robert Piwowarek <robpiwowarek@gmail.com>
2015-11-08 10:43:41 +0100

    Initial Commit

---
+++ 1024jam/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        float input = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(input * 1000 * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
---
+++ 1024jam/Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+    public GameObject missile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missile, new Vector3(transform.position.x, transform.position.y+1), Quaternion.identity);
+        }
+	}
+}
