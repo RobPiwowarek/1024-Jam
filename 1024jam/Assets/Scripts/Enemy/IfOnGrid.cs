7f76ebff104bb610210e8ca52f3818796e7e5029
Robert Piwowarek <robpiwowarek@gmail.com>
2015-11-11 21:57:47 +0100

    ItStops
    filip plz

--- 1024jam/Assets/Scripts/Enemy/IfOnGrid.cs
+++ 1024jam/Assets/Scripts/Enemy/IfOnGrid.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class IfOnGrid : MonoBehaviour {
-	public GameObject Game_World;
 	// Use this for initialization
-	void Start () {
-		if (Game_World.GetComponent<Game_Continuity> ().grid == true) {
-			this.GetComponent<Moving>().enabled = false;
+	void Awake () {
+        Debug.Log("DUPA");
+		if (GameObject.FindGameObjectWithTag("GW").GetComponent<Game_Continuity>().grid) {
+            this.GetComponent<Moving>().enabled = false;
+            Debug.Log(GetComponent<Moving>().enabled);
 		}
 	}
 
