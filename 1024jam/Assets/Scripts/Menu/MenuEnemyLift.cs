ca835895786882eea7908936e06b1caaa824d2b6
Robert Piwowarek <robpiwowarek@gmail.com>
2015-11-11 14:25:09 +0100

    Menu Visual Concept

--- 1024jam/Assets/Scripts/Menu/MenuEnemyLift.cs
+++ 1024jam/Assets/Scripts/Menu/MenuEnemyLift.cs
@@ -6,6 +6,6 @@ public class MenuEnemyLift : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        col.gameObject.transform.position = new Vector3(col.gameObject.transform.position.x, col.gameObject.transform.position.y + 50, col.gameObject.transform.position.z);
+        col.gameObject.transform.position = new Vector3(col.gameObject.transform.position.x, col.gameObject.transform.position.y + 150, col.gameObject.transform.position.z);
     }
 }
