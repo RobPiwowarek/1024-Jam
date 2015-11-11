6e15256b6093b7dec5aa949248df019882d5683c
Robert Piwowarek <robpiwowarek@gmail.com>
2015-11-11 15:07:14 +0100

    Menu Update
    Button highlight
    added scenes to build settings.

--- 1024jam/Assets/Scripts/Menu/Menu.cs
+++ 1024jam/Assets/Scripts/Menu/Menu.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool isQuitButton = false;
+    public bool isPlayButton = false;
+    public bool isOptionsButton = false;
+    public bool isCreditsButton = false;
+
+    void OnMouseOver()
+    {
+        GetComponent<TextMesh>().color = new Color(255, 0, 0);
+    }
+
+    void OnMouseExit() {
+        GetComponent<TextMesh>().color = new Color(255, 255, 255);
+    }
+
+    void OnMouseUp()
+    {
+        if (isQuitButton)
+            Application.Quit();
+        else if (isPlayButton)
+            Application.LoadLevel(1);
+        else if (isOptionsButton) ;
+        else if (isCreditsButton) ;
+
+    }
 }
