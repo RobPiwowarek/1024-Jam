c6f443061eb2382f5762761d7ddd79e4398496e4
Ardihkon <filip.wasilewski96@gmail.com>
2015-11-16 18:58:54 +0100

    Debris WIP
    Not flying but rotating, will fix later

---
+++ 1024jam/Assets/Scripts/Debris/Debris_Action.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debris_Action : MonoBehaviour {
+
+	public float lifetime = 5;
+	// Use this for initialization
+	void Start () {
+
+
+		foreach(Transform el in transform){
+			el.GetComponent<Rigidbody2D>().angularDrag = Random.Range(0.1f,0.75f);
+			el.GetComponent<Rigidbody2D>().AddTorque(Random.Range(15.0f,60.0f));
+		}
+
+
+
+	}
+
+	void Update(){
+		lifetime -= Time.deltaTime;
+		if (lifetime < 0)
+			Destroy(this.gameObject);
+	}
+}
--- 1024jam/Assets/Scripts/Enemy/Death.cs
+++ 1024jam/Assets/Scripts/Enemy/Death.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Death : MonoBehaviour {
 
+
+
 	void OnTriggerEnter2D(Collider2D col){
         if ((col != null)){
             MissileMovement mov = col.gameObject.GetComponent<MissileMovement>();
             if ((mov != null) && (this.GetComponent<EnemyMovement>().ID == mov.ID)){
                 Destroy(col.gameObject);
                 Destroy(this.gameObject);
+
             }
             else{
                 PlayerStatistics playerStats = col.gameObject.GetComponent<PlayerStatistics>() ;
