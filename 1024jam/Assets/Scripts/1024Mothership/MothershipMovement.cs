78533f3af7ea95277700e05f6150b4e327d7d6dd
Ardihkon <filip.wasilewski96@gmail.com>
2015-12-21 21:50:14 +0100

    Mothership More
    Moar Sry for delay plz

--- 1024jam/Assets/Scripts/1024Mothership/MothershipMovement.cs
+++ 1024jam/Assets/Scripts/1024Mothership/MothershipMovement.cs
@@ -2,24 +2,43 @@
 using System.Collections;
 
 public class MothershipMovement : MonoBehaviour {
+	//Mothership Behaviour/life
+	public int MothershipLife = 10000;
+	int FightScenario = 0;
+
+	//Mothership Movement
 	public float treshold = 0.15f;
 	public float smoothness = 0.5f;
 	public float DescendMovement = 5.0f;
-	int FightScenario = 0;
-	Vector2 target = new Vector2(0.0f,0.0f);
+	Vector2 target = new Vector2(0.0f,5.0f);
+
+	//Mines
+	public GameObject Mine;
+
+
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine ("Mothership_Descend");
 	}
 
 	IEnumerator Mothership_Descend(){
-		while (transform.position.y>5.0f) {
-			float w = Time.deltaTime*DescendMovement;
-			transform.position = new Vector2(transform.position.x,transform.position.y-w);
+		while (Vector2.Distance(transform.position,target)>1.0f) {
+			//float w = Time.deltaTime*DescendMovement;
+			//transform.position = new Vector2(transform.position.x,transform.position.y-w);
+			transform.position = Vector2.Lerp(transform.position,target,Time.deltaTime*DescendMovement);
 			yield return null;
 		}
 		yield return new WaitForSeconds (3.0f);
 		StartCoroutine ("Mothership_Fight");
+		StopCoroutine ("Mothership_Descend");
+	}
+
+	 public void Got_Hit(int damage){
+		MothershipLife -= damage;
+		if (MothershipLife < 0) {
+			Destroy(this.gameObject);
+		}
 	}
 
 	IEnumerator Mothership_Fight(){
@@ -35,15 +54,31 @@ public class MothershipMovement : MonoBehaviour {
 		}
 
 		case 1:{
+			target = new Vector2(0.0f,15.0f);
+			while(Vector2.Distance(transform.position,target)>treshold){
+				transform.position = Vector2.Lerp(transform.position,target,Time.deltaTime*smoothness*3.0f);
+				yield return null;
+			}
+
+			Instantiate(Mine,new Vector2(transform.position.x,transform.position.y),Quaternion.identity);
+
+			yield return new WaitForSeconds(1.0f);
+			target = new Vector2(Random.Range(-1.16f,1.16f),Random.Range(4.0f,7.0f));
+			while(Vector2.Distance(transform.position,target)>treshold){
+				transform.position = Vector2.Lerp(transform.position,target,Time.deltaTime*smoothness);
+				yield return null;
+			}
+			/*
 			yield return new WaitForSeconds(Random.Range(1.0f,4.0f));
 			Debug.Log("Ratata!");
 			yield return new WaitForSeconds(Random.Range(1.0f,4.0f));
-			Debug.Log("Ratata2!");
+			Debug.Log("Ratata2!");*/
 			break;
 
 		}
 
 		}
+
 		FightScenario = Random.Range (0,1);
 		StartCoroutine ("Mothership_Fight");
 		}
---
+++ 1024jam/Assets/Scripts/Mothership_Missile1/MothershipMissile1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MothershipMissile1 : MonoBehaviour {
+
+	public float speed = 0.25f;
+	public float multiplier = 1.25f;
+	public float XMovement = 0;
+	public float XMultiplier = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Attack");
+		XMovement = -0.35f*XMultiplier;;//Random.Range (-0.35f,0.35f)*XMultiplier;
+	}
+
+	IEnumerator Attack(){
+
+		while (true) {
+			transform.position = new Vector2(transform.position.x+XMovement,transform.position.y-speed*Time.deltaTime);
+			speed *= multiplier;
+			yield return null;
+		}
+	}
+
+}
